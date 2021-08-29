41c3418c96439fad617c0d3a611cae4a8756c090
Arvib Fazriansyah <85182275+arv-fazriansyah@users.noreply.github.com>
2021-08-29 13:50:21 +0700

    Create gdrive2.js

---
+++ gdrive2.js
@@ -0,0 +1,24 @@
+<style>
+  h1 { text-align:center; }
+</style>
+<center>
+<label class="form-label"></label>
+<div class="input-group mb-3">
+  <span class="input-group-text jt-btn-primary" id="basic-addon3">URL</span>
+  <input aria-describedby="basic-addon3" class="form-control bg-transparent jt-border-light text-reset" id="driveID" placeholder="https://drive.google.com/file/d/1QQhvBos-jafmxxxx-6n8upYS38Gp_OE7/view?usp=sharing" type="text" />
+</div>
+
+  <button class="btn jt-btn-primary" id="get-button" type="submit">Create Direct Link</button>
+
+<br />
+<label class="form-label"></label>
+<div class="input-group mb-3">
+  <span class="input-group-text jt-btn-primary" id="basic-addon3">Result</span>
+  <input aria-describedby="basic-addon3" class="form-control bg-transparent jt-border-light text-reset" id="output" type="text" />
+</div>
+
+  <button class="btn jt-btn-primary mb-2 me-2" id="copy" type="button">Copy</button>
+  <button class="btn jt-btn-primary mb-2 me-2" id="download" type="button">Download</button>
+  <button class="btn jt-btn-primary mb-2 me-2" id="reset" type="button">Clear</button>
+
+ </center>
